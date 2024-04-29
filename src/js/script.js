c0d139fe5b75ef5d4ee982ffcd6a0f203f6a8265
Alice <alicesb2006@gmail.com>
2024-04-29 13:04:28 -0300

    Modificando o estilo do site

--- src/js/script.js
+++ src/js/script.js
@@ -2,31 +2,38 @@ let tarefas = []
 
 function validaCampo(){
     let valida = false;
-    if(document.getElementById("task").value == "") valida = true;
-    return valida
+    if (document.getElementById("task").value == "") valida = true;
+    return valida;
 }
 
 function adicionarTarefa(){
     let linhas = document.getElementById("task");
-    if(validaCampo){
-        alert("Preencha o Campo com a tarefa");
-    }else{
+    
+    if(validaCampo()){
+        alert("Preencha o Campo com a tarefa")
+    }
+    else{
         tarefas.push(linhas.value);
-        linhas.value = "";
+        linhas.value ="";
         listarTarefas();
     }
-    document.getElementById("task").focus();
+        document.getElementById("task").focus();
 }
 
+//função listar tarefas 
+
 function listarTarefas(){
-    let valor = "";
-    for(let i = 0; i < tarefas.lenght; i++){
-        valor += tarefas[i] + "<br>";
+    let valor ="";
+    for(let i=0; i< tarefas.length; i++){
+        valor += tarefas[i] +"<br>";
     }
     document.getElementById("lista").innerHTML = valor;
 }
+//função de remover tarefas
 
 function removerTarefa(){
+    //metodo pop = remove o ultimo item adicionado
     tarefas.pop();
     listarTarefas();
+
 }
